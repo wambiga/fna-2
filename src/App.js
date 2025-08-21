de5789d2a17b914782e1a2bd88c4fdc671c506df
wambiga <wambiga@gmail.com>
2025-08-21 15:07:51 +0300

    xlsx

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useRef, useEffect } from 'react';
+import React, { useState, useMemo, useRef } from 'react';
 import html2pdf from 'html2pdf.js';
 import './App.css';
 
